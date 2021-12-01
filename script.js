b7868011894b6b33484f698df36f2ec531c5995d
jitendra19 <jiten.cs2010@gmail.com>
2021-12-01 23:05:21 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello!')
+console.log('asdf');
+document.querySelectorAll('button').forEach((btn) => {
+  console.log('btn');
+  alert('btn');
+  let counter = 0;
+  let name = btn.innerHTML;
+  btn.addEventListener('click', () => {
+    counter++;
+    console.log(`${name} --- ${counter}`);
+    btn.innerHTML = name + counter;
+    // return counter;
+  });
+});
